367472b936b8adfd34f54c0a3ae34625711cbb06
stefaniarotaru <rotaru.g.stefania@gmail.com>
2021-12-24 19:03:10 +0200

    add links in mobile dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -7,10 +7,10 @@ const Dropdown = ({isOpen, toggle}) => {
             ? 'absolute w-full grid grid-rows-4 text-center bg-white/90 items-center z-50 mt-[5rem] font-rubik font-bold' 
             : 'hidden'}
             onClick={toggle}>
-            <a href='#' className="p-4 text-orange-400 hover:text-[#EE4D47]" >ABOUT</a>
-            <a href='#' className="p-4 text-orange-400 hover:text-[#EE4D47]" >HOW TO</a>
-            <a href='#' className="p-4 text-orange-400 hover:text-[#EE4D47]" >FAQ</a>
-            <a href='#' className="p-4 text-orange-400 hover:text-[#EE4D47]" >CONTACT US</a>
+            <a href='#about' className="p-4 text-orange-400 hover:text-[#EE4D47]" >ABOUT</a>
+            <a href='#howto' className="p-4 text-orange-400 hover:text-[#EE4D47]" >HOW TO</a>
+            <a href='#faq' className="p-4 text-orange-400 hover:text-[#EE4D47]" >FAQ</a>
+            <a href='#contact' className="p-4 text-orange-400 hover:text-[#EE4D47]" >CONTACT US</a>
 
         </div>
     )
